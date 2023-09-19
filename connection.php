20b7918f950ffdb3e0c505c5bb0344949a3833aa
Ahmed Mahmoud Turki <100971093+Aturkii@users.noreply.github.com>
2023-09-19 08:12:11 +0300

    connection.php

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $db_name = "reg_form";  
+    $db_name = "login";  
     $conn = new mysqli($servername, $username, $password, $db_name);
     if($conn->connect_error){
         die("Connection failed".$conn->connect_error);
